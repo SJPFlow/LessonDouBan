cb528ffc5fc19d80cc74ecc3e8f4cfedc295c2f2
lanou <zhaoce@lanou3g-3.local>
2016-07-11 12:49:24 +0800

    测试工程

---
+++ ggg/ggg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ggg
+//
+//  Created by lanou3g on 16/7/11.
+//  Copyright © 2016年 sjp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
